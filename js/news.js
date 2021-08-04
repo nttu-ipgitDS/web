5230deeab3f8acd35d59dfd04cb0bb8f41128f2e
hsiyu <61860935+hsiyu56@users.noreply.github.com>
2021-08-04 22:53:12 +0800

    1.0.0

---
+++ js/news.js
@@ -0,0 +1,30 @@
+//變數宣告
+const news1 = document.getElementById("index1");
+const news11 = document.getElementById("index1date");
+const news2 = document.getElementById("index2");
+const news22 = document.getElementById("index2date");
+const news3 = document.getElementById("index3");
+const news33 = document.getElementById("index3date");
+const news4 = document.getElementById("index4");
+const news44 = document.getElementById("index4date");
+const news5 = document.getElementById("index5");
+const news55 = document.getElementById("index5date");
+
+//輸入更改的文字
+news1.innerHTML = "● 系學會網站成立了!";
+news11.innerHTML = "2021/07/31";
+news2.innerHTML = "";
+news22.innerHTML = "";
+news3.innerHTML = "";
+news33.innerHTML = "";
+news4.innerHTML = "";
+news44.innerHTML = "";
+news5.innerHTML = "";
+news55.innerHTML = "";
+
+//輸入更改好的網頁
+news1.href = "";
+news2.href = "";
+news3.href = "";
+news4.href = "";
+news5.href = "";
